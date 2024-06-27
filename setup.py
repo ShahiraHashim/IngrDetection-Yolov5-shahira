fdd1633c164b01eb58d38b1f6a5e3ca14c183821
ShahiraHashim <you@example.com>
2024-06-28 05:00:51 +0800

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ingrDetection',
+    version= '0.0.0',
+    author= 'ShahiraHashim',
+    author_email= 'nurshahirahashim0@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
